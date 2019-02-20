411d0682e853acd1a42d723c58eedcf4b7e1e8fa
yinghao666 <1017945875@qq.com>
2019-02-20 19:36:34 +0800

    Merge branch 'master' of https://github.com/chenyinghao199814/1807a
    # Conflicts:
    #	src/main/java/com/jk/controller/Fs/haoge.java

--- src/main/java/com/jk/controller/Fs/haoge.java
+++
@@ -1,9 +0,0 @@
-package com.jk.controller.Fs;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-@Controller
-@ResponseBody
-public class haoge {
-}
